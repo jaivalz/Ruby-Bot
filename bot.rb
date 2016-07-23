40364834cd3a8bd69e782c2d4790142679385d23
Jaime <jaivalz@yahoo.com>
2016-07-23 14:15:01 +0200

    Modified bot rb class for possible responses and more

--- bot.rb
+++ bot.rb
@@ -23,9 +23,41 @@ class Bot
 
   def response_to(input)
     prepared_input = preprocess(input).downcase
+    sentence = best_sentence(prepared_input)
+    responses = possible_responses(sentence)
+    responses[rand(responses.length)]
   end
 
+
   private
+
+  def possible_responses(sentence)
+    responses = []
+      @data[:responses].keys.each do |pattern|
+        next unless pattern.is_a?(String)
+          if sentence.match('\b' + pattern.gsub(/\*/, '') + '\b')
+            if pattern.include?('*')
+            responses << @data[:responses][pattern].collect do |phrase|
+          matching_section = sentence.sub(/^.*#{pattern}\s+/, '')
+        phrase.sub('*', WordPlay.switch_pronouns(matching_section))
+      end
+    else
+        responses << @data[:responses][pattern]
+      end
+    end
+  end
+    responses << @data[:responses][:default] if responses.empty?
+    responses.flatten
+  end
+
+
+
+  def best_sentence(input)
+    hot_words = @data[:responses].keys.select do |k|
+    k.class == String && k =~ /^\w+$/
+  end
+    WordPlay.best_sentence(input.sentences, hot_words)
+  end
 
   def preprocess(input)
     perform_substitutions input
