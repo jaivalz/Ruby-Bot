e9e063c8c791f2206c40c00b50079a891742fb25
Jaime <jaivalz@yahoo.com>
2016-07-23 10:43:50 +0200

    Added input user's input acceptation

--- bot.rb
+++ bot.rb
@@ -21,7 +21,20 @@ class Bot
     random_response :farewell
   end
 
+  def response_to(input)
+    prepared_input = preprocess(input).downcase
+  end
+
   private
+
+  def preprocess(input)
+    perform_substitutions input
+  end
+
+  def perform_substitutions(input)
+    @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
+    input
+  end
 
   def random_response(key)
     random_index = rand(@data[:responses][key].length)
